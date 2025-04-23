3e109c804444544f409563e65950f45dc5b8f685
Vembe Sajila Rajil <svembe@gmail.com>
2025-04-23 00:31:14 -0400

    Working environment set

---
+++ 1-leetcode/main.py
@@ -0,0 +1,10 @@
+# Main
+
+from problem1 import Solution  # Fixed import statement
+
+sol = Solution()  # Corrected instantiation
+print(sol.CountLargestGroup(1))  # Updated method name to match the class definition
+
+
+
+
---
+++ 1-leetcode/problem1.py
@@ -0,0 +1,7 @@
+# 1399. Count Largest Group
+# Easy
+
+class Solution:
+	def CountLargestGroup(self, n: int) -> int:
+		return "hello, world !"
+
